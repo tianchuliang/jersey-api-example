9d1bc15e67227f66e65ce4811b408d9f61c17c38
Tianchu Liang <liangtianchu@gmail.com>
2015-10-25 00:41:00 -0400

    UserWebService.java pretty much finished

---
+++ src/main/java/com/jking31cs/jerseyexample/webservices/UserWebService.java
@@ -0,0 +1,66 @@
+package com.jking31cs.jerseyexample.webservices;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jking31cs.jerseyexample.objects.User;
+import com.jking31cs.jerseyexample.stores.UserStore;
+
+@Path("api/users")
+public class UserWebService{
+	private final UserStore store;
+
+	@Inject 
+	public UserWebService(UserStore store){
+		this.store=store;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getAllUsers(){
+		return store.getAll();
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUser(@PathParam("id") Long id){
+		return store.get(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User saveNewUser(User user){
+		return store.save(user);
+	}
+
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User updateUser(@PathParam("id") Long id, User user){
+		return store.save(user);
+		// User old = store.get(id);
+		// old.setName(user.getName());
+		// old.setEmail(user.getEmail());
+	}
+
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User deleteUser(@PathParam("id") Long id){
+		return store.delete(store.get(id));
+	}
+	
+}
