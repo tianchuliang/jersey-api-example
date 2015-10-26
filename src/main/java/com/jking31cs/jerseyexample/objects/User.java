d821892c693fe0086ad8c16910d41eec25cbdfb0
Tianchu Liang <liangtianchu@gmail.com>
2015-10-25 23:51:34 -0400

    Modified User.java to have correct Json annotations

--- src/main/java/com/jking31cs/jerseyexample/objects/User.java
+++ src/main/java/com/jking31cs/jerseyexample/objects/User.java
@@ -18,6 +18,12 @@ public class User {
     private String name;
     private String email;
 
+    private User(){
+        this.id=null;
+        this.name=null;
+        this.email=null;
+    }
+
     @JsonCreator
     public User(
         @JsonProperty("id") Long id,
